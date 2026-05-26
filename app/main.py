6688ab05ade13cd6281091f167d0f82910c94c00
Eyyub01 <abbaszadeeyyub@gmail.com>
2026-05-26 21:48:36 +0200

    bug fixes, vercel added

--- app/main.py
+++ app/main.py
@@ -16,6 +16,7 @@ app = FastAPI(title="Voice Task Manager")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
